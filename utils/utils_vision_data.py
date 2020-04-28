5ffd5237ba4c49685aa5c115c50b98dad1001432
Léonard Boussioux <leonard.boussioux@student.ecp.fr>
2020-04-28 19:58:32 -0400

    small bug with path

--- utils/utils_vision_data.py
+++ utils/utils_vision_data.py
@@ -165,6 +165,7 @@ def create_dataset(min_wind, min_steps, max_steps, vision_name, y_name, path = '
     vision_data = extract_vision(data, epsilon=0.05)
     y, _ = prepare_tabular_data_vision(min_wind=min_wind,
                                         min_steps=min_steps,
-                                        max_steps=max_steps)
+                                        max_steps=max_steps,
+                                        path = path)
     np.save("data/" + vision_name, vision_data, allow_pickle=True)
     np.save("data/" + y_name, y, allow_pickle=True)
