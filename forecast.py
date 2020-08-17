971e6dc349b25c4b0b0be041bc6bfffc03f79d42
cynthiazeng <cynthiazeng@CynthiasMacBook.homenet.telecomitalia.it>
2020-08-17 12:39:16 -0400

    changes to forecast notebook

--- forecast.py
+++ forecast.py
@@ -3,6 +3,6 @@
 import numpy as np
 from utils.utils_vision_data import * ; from utils.data_processing import * ;
 
-y, _, y_with_timestamp = prepare_tabular_data_vision(min_wind=34, min_steps=20, max_steps=120, path = 'since1980_downloaded_07_08_20.csv')
+y, _= prepare_tabular_data_vision(min_wind=34, min_steps=20, max_steps=120, path = 'since1980.csv')
 
 np.save('./data/y_1980_34_20_120_forecast_test.npy', y, allow_pickle=True)
